8c178fa72f8b4aa024e418028f5fa4296be0ebe1
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-10-17 17:38:32 +0200

    update lintr file

--- R/macurves.R
+++ R/macurves.R
@@ -7,7 +7,7 @@
 #' @examples
 #' macurves("S3X3")
 #' @export
-macurves <- function(seas_filter = c("S3X3", "S3X1", "S3X5", "S3X9", "S3X15"), period = 12){
+macurves <- function(seas_filter = c("S3X3", "S3X1", "S3X5", "S3X9", "S3X15"), period = 12) {
   seas_filter <- match.arg(toupper(seas_filter),
                           c("S3X3", "S3X1", "S3X5", "S3X9", "S3X15"))
   seas_opt <- .jcall("jdplus/x12plus/base/api/SeasonalFilterOption",
